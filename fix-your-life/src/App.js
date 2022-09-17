d053e32ae9d93c236cc4b1921c85a570978d85a4
Kieran Dunch <dunch.kieran@gmail.com>
2022-09-17 14:24:54 -0400

    added  sidebar component

--- fix-your-life/src/App.js
+++ fix-your-life/src/App.js
@@ -8,12 +8,16 @@ import Create from './pages/create/Create'
 import Login from './pages/login/Login'
 import Signup from './pages/signup/Signup'
 import Project from './pages/project/Project'
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <div className="container">
+          <Navbar />
+          <Sidebar />
           <Switch>
             <Route path='/'>
               <Dashboard />
