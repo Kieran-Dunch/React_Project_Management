c4a32f9e5b2d3d204e4f6f69e7d2a8798f688ee7
Kieran Dunch <dunch.kieran@gmail.com>
2022-09-17 14:25:11 -0400

    added styling

---
+++ fix-your-life/src/components/Sidebar.js
@@ -0,0 +1,34 @@
+// styles & images
+import './Sidebar.css'
+import DashboardIcon from '../assets/dashboard_icon.svg'
+import AddIcon from '../assets/add_icon.svg'
+import { NavLink } from 'react-router-dom'
+
+export default function Sidebar() {
+  return (
+    <div className='sidebar'>
+      <div className="sidebar-content">
+        <div className="user">
+          {/* avatar and username */}
+          <p>Hey user</p>
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <NavLink to='/'>
+                <img src={DashboardIcon} alt="dashboard icon" />
+                <span>Dashboard</span>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to='/create'>
+                <img src={AddIcon} alt="Add project icon" />
+                <span>New project</span>
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  )
+}
