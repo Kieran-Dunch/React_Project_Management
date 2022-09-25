709d1b6e77bd92fb1edba833398b9c79db3f0796
Kieran Dunch <dunch.kieran@gmail.com>
2022-09-25 19:46:41 -0400

    added image upload to signup hook

--- fix-your-life/src/firebase/config.js
+++ fix-your-life/src/firebase/config.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
+import 'firebase/storage'
 
 const firebaseConfig = {
   apiKey: "AIzaSyBo9RT80_OFXCLTT1f76kQTVEXxStmuCNg",
@@ -17,8 +18,9 @@ firebase.initializeApp(firebaseConfig)
 // initialize services
 const projectFirestore = firebase.firestore()
 const projectAuth = firebase.auth()
+const projectStorage = firebase.storage()
 
 // timestamp
 const timestamp = firebase.firestore.Timestamp
 
-export { projectFirestore, projectAuth, timestamp }
+export { projectFirestore, projectAuth, projectStorage, timestamp }
