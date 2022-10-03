9a6b00e523527817b5c8ce0cdff9fbffcb069b9f
Kieran Dunch <dunch.kieran@gmail.com>
2022-10-03 15:15:35 -0600

    164

--- fix-your-life/src/pages/login/Login.js
+++ fix-your-life/src/pages/login/Login.js
@@ -1,6 +1,42 @@
+// styles
+import { useState } from 'react'
+import './Login.css'
+
 
 export default function Login() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(email, password)
+  }
+
   return (
-    <div>Login</div>
+    <form className="auth-form" onSubmit={handleSubmit}>
+      <h1>Sign up</h1>
+      <label>
+        <span>email:</span>
+        <input
+          type="email"
+          required
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input
+          type="password"
+          required
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <button>Login</button>
+      {/* {!isPending && <button className='btn'>Login</button>}
+      {isPending && <button className='btn' disabled>Loading</button>}
+      {error && <div className="error">{error}</div>} */}
+    </form>
   )
 }
