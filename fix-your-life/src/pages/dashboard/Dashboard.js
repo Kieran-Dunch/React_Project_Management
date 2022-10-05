7b632ee75061fe517ae66e12820dfe7f2d82423d
Kieran Dunch <dunch.kieran@gmail.com>
2022-10-05 14:27:44 -0600

    177 adding  project list

--- fix-your-life/src/pages/dashboard/Dashboard.js
+++ fix-your-life/src/pages/dashboard/Dashboard.js
@@ -1,6 +1,13 @@
+import ProjectList from '../../components/ProjectList'
+import { useCollection } from '../../hooks/useCollection'
 
-export default function dashboard() {
+export default function Dashboard() {
+  const { documents, error } = useCollection('projects')
   return (
-    <div>dashboard</div>
+    <div>
+      <h2 className="page-title">Dashboard</h2>
+      {error && <p className="error">{error}</p>}
+      {documents && <ProjectList projects={documents} />}
+    </div>
   )
 }
